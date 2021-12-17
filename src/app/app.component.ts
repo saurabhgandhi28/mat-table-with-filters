15b801aa356a4958516ed3faa5c98f12ef372f44
Saurabh Gandhi <48408965+saurabhgandhi07@users.noreply.github.com>
2021-12-17 16:23:16 +0530

    Data Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,27 @@
 import { Component } from '@angular/core';
 
+export interface MoenyHiest {
+  id: number;
+  name: string;
+  character:string;
+  played_by:string;
+  occupation:string;
+  gender:string;
+}
+
+const DATA: MoenyHiest[] = [
+  {id: 1, name: 'Serjio Marquina', character: 'Professor',played_by: 'Alvaro Morte',occupation: 'Heist Planner',gender: 'Male'},
+  {id: 2, name: 'Andres de Fonollosa', character: 'Berlin',played_by: 'Pedro Alonsa',occupation: 'Robber',gender: 'Male'},
+  {id: 3, name: 'Silene Oliveira', character: 'Tokyo',played_by: 'Ursula Corbera',occupation: 'Robber',gender: 'Female'},
+  {id: 4, name: 'Agata Jimenez', character: 'Nairobi',played_by: 'Alba Flores',occupation: 'Robber',gender: 'Female'},
+  {id: 5, name: 'Raquel Fuentes', character: 'Lisbon',played_by: 'Itziar Ituno',occupation: 'Police, Robber',gender: 'Female'},
+  {id: 6, name: 'Daniel Ramos', character: 'Denver',played_by: 'Jaime Lorente',occupation: 'Robber',gender: 'Male'},
+  {id: 7, name: 'Anibal Cortes', character: 'Rio',played_by: 'Miguel Herran',occupation: 'Robber',gender: 'Male'},
+  {id: 8, name: 'Agustin Ramos', character: 'Moscow',played_by: 'Paco Tous',occupation: 'Robber',gender: 'Male'},
+  {id: 9, name: 'Mirko Dragic', character: 'Helsinki',played_by: 'Darko Peric',occupation: 'Robber',gender: 'Male'},
+  {id: 10, name: 'Radko Dargic', character: 'Oslo',played_by: 'Roberto Garcia',occupation: 'Robber',gender: 'Male'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +29,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mat-table-with-filters';
+  displayedColumns: string[] = ['id','character','name','played_by','occupation','gender'];
+  characters = DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSliderModule } from '@angular/material/slider';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSliderModule } from '@angular/material/slider';
+import {MatTableModule} from '@angular/material/table';
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +19,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule,
+    MatToolbarModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSliderModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
