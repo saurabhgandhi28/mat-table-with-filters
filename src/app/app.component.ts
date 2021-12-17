1e446165c0fba6b76d103af5f717437e693bb739
Saurabh Gandhi <48408965+saurabhgandhi07@users.noreply.github.com>
2021-12-17 21:27:30 +0530

    Filter added for Id column

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export interface MoenyHiest {
   id: number;
@@ -27,8 +27,63 @@ const DATA: MoenyHiest[] = [
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  ngOnInit(): void {
+    this.filtredCharacters = this.characters;
+    console.log(this.filtredCharacters)
+  }
+  
   title = 'mat-table-with-filters';
   displayedColumns: string[] = ['id','character','name','played_by','occupation','gender'];
-  characters = DATA;
+  characters:any = DATA;
+  filtredCharacters:any = []; 
+  searchValue: any = {};
+  isIdApplied: boolean = false;
+
+
+  applyFilter(event?: any) {
+    if (event) {
+      event.target.parentElement.onclick = null;
+      event.target.parentElement.click();
+    }
+    this.filtredCharacters = JSON.parse(JSON.stringify(this.characters));
+    Object.keys(this.searchValue).forEach(key => {
+      this.filtredCharacters = this.filtredCharacters.filter((character) =>
+        character[key].toString().toLowerCase().includes(this.searchValue[key].toLowerCase())
+      )
+    });
+    if (this.searchValue.id) {
+      this.isIdApplied = true;
+    }
+  }
+
+  clearColumn(columnKey: string): void {
+    this.clearFilterApplied(columnKey);
+    delete this.searchValue[columnKey];
+    this.filtredCharacters = JSON.parse(JSON.stringify(this.characters));
+    Object.keys(this.searchValue).forEach(key => {
+      this.filtredCharacters = this.filtredCharacters.filter((character) =>
+        character[key].toString().toLowerCase().includes(this.searchValue[key].toLowerCase())
+      )
+    });
+  }
+
+  clearFilterApplied(columnKey: string): void {
+    if (columnKey == 'id') {
+      this.isIdApplied = false;
+       }
+      //else if (columnKey == 'memberName'){
+      //     this.isMemberNameApplied = false;
+      // }else if(columnKey == 'fuelMovementType'){
+      //     this.isfuelMovementTypeApplied = false;
+      // }else if(columnKey == 'fuelProductName'){
+      //     this.isfuelProductNameApplied = false;
+      // }else if(columnKey == 'netGallons'){
+      //     this.isNetGallonsApplied = false;
+      // }
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -11,16 +12,20 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatSliderModule } from '@angular/material/slider';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { FilterItemDirective } from './filter-item.directive';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterItemDirective
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatFormFieldModule,
+    MatInputModule,
     MatCardModule,
     MatSelectModule,
     MatToolbarModule,
@@ -28,7 +33,8 @@ import {MatTableModule} from '@angular/material/table';
     MatButtonModule,
     MatIconModule,
     MatSliderModule,
-    MatTableModule
+    MatTableModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
